37180ff15e1420940046d42aef1dc3971efbd958
Renat <83014719+renatts@users.noreply.github.com>
2021-11-01 16:10:21 +0200

    fixed logic

--- pass-validator.ps1
+++ pass-validator.ps1
@@ -1,6 +1,6 @@
 $password = $args[0]
 $file = $args[1]
-$OK = 0
+$ERROR = 0
 
 if ( $args[0] -eq "-f") {
     $password = Get-ChildItem $file -Recurse -Include *.txt | Get-Content
@@ -11,36 +11,34 @@ else {
 
 # Includes 10 or more chars validation
 if ($password.Length -lt 10) {
-    write-host -ForegroundColor red "ERROR: Password is shorter than 10 characters" 
+    $ERROR = 1
+    Write-Host -ForegroundColor red "ERROR: Password is shorter than 10 characters" 
 }
-else {
-    $OK++;
-}  
 
 # Includes capitals validation
-if ($password -cmatch "[A-Z]") { $OK++; }
-else {
-    write-host -ForegroundColor red "ERROR: Password doesn't include capitals"
+if ( ! ($password -cmatch "[A-Z]")) {
+    $ERROR = 1
+    Write-Host -ForegroundColor red "ERROR: Password doesn't include capitals"
 }
 
 # Includes lowercase letters validation
-if ($password -cmatch "[a-z]") { $OK++; }
-else {
-    write-host -ForegroundColor red "ERROR: Password doesn't include lowercase letters"
+if ( ! ($password -cmatch "[a-z]")) { 
+    $ERROR=1
+    Write-Host -ForegroundColor red "ERROR: Password doesn't include lowercase letters"
 }
 
 # Includes digits validation
-if ($password -cmatch "[0-9]") { $OK++; }
-else {
-    write-host -ForegroundColor red "ERROR: Password doesn't include digits"
+if ( ! ($password -cmatch "[0-9]")) { 
+    $ERROR=1
+    Write-Host -ForegroundColor red "ERROR: Password doesn't include digits"
 }
 
 # Check OK status, if OK isn't equal to 4, means he isn't passed all 4 validations
-if ( $OK -eq 4) {
-    write-host -ForegroundColor green "Your password is correct"
-    exit 0;
+if ( $ERROR -eq 1) {
+    Write-Host -ForegroundColor red "Bad Password..."
+    exit 1;
 }
 else {
-    write-host -ForegroundColor red "Bad Password..."
-    exit 1;
+    Write-Host -ForegroundColor green "Your password is correct"
+    exit 0;
 }
